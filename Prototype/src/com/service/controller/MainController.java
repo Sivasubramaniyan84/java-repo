4e74248776bc02a689f40ea95b3ea0e538f55645
Sivasubramaniyan <sivasubramaniyan.it@gmail.com>
2019-03-11 22:43:58 +0530

    Changes Comitted

--- Prototype/src/com/service/controller/MainController.java
+++ Prototype/src/com/service/controller/MainController.java
@@ -3,7 +3,7 @@ package com.service.controller;
 public class MainController {
 
 	public static void main(String[] args) {
-		System.out.println("This is a Sample Java Project");
+		System.out.println("Sample Java Project...");
 
 	}
 
