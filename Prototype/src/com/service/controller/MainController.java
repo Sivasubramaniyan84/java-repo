a344f6770b83103293e6a90b86a7dc05e61cd22a
Sivasubramaniyan <sivasubramaniyan.it@gmail.com>
2019-03-11 23:50:35 +0530

    Changes added in MainController

--- Prototype/src/com/service/controller/MainController.java
+++ Prototype/src/com/service/controller/MainController.java
@@ -3,7 +3,7 @@ package com.service.controller;
 public class MainController {
 
 	public static void main(String[] args) {
-		System.out.println("Sample Java Project...");
+		System.out.println("Prototype Java Project...");
 
 	}
 
